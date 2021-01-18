e5aa67f01a9f9f0c773a5e36ca690fbb0f72078d
Esteban Islas <1717110261@utectulancingo.edu.mx>
2021-01-18 17:41:24 -0600

    Agregando archivos de conexion a localhost de la api (no es necesario en produccion)

--- app/src/main/java/com/islasoft/atstore/common/Constantes.java
+++ app/src/main/java/com/islasoft/atstore/common/Constantes.java
@@ -3,6 +3,6 @@ package com.islasoft.atstore.common;
 public class Constantes {
 
     //Constantes que se utilizan de forma global
-    public static final String BASE_URL = "http://127.0.0.1:3000";
+    public static final String BASE_URL = "http://192.168.1.68:3000"; //192.168.1.68
 
 }
