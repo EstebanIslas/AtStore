e365a7487a3cae78d21c519e940f954d13d70ee2
Esteban Islas <1717110261@utectulancingo.edu.mx>
2021-01-18 16:54:20 -0600

    Vinculando mejoras al acceso del Contexto de la app

--- app/src/main/java/com/islasoft/atstore/common/MyApp.java
+++ app/src/main/java/com/islasoft/atstore/common/MyApp.java
@@ -1,7 +1,26 @@
 package com.islasoft.atstore.common;
 
 import android.app.Application;
+import android.content.Context;
 
+//Mejorar el acceso al contexto
 public class MyApp extends Application {
-    
+    private static MyApp instance;
+
+    //Devuelve la isntancia creada instance
+    public static MyApp getInstance(){
+        return instance;
+    }
+
+    //Obtener el context de la app
+    public static Context getContext(){
+        return instance;
+    }
+
+    //Se crea solo una vez cuando se abre la app
+    @Override
+    public void onCreate() {
+        instance = this;
+        super.onCreate();
+    }
 }
